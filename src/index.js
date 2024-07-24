bdd9aeb0dc078dd0bfaddf92544710d02d3df3ca
reyleal27 <reyvincentleal@gmail.com>
2024-07-25 07:54:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/expensetracker">
+    <BrowserRouter basename="/expensetracker/">
     <App />
     </BrowserRouter>
   </React.StrictMode>
